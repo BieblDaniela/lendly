777c00d6bf613d67dd6f447d21bf5254f7d73aa6
BieblDaniela <biebl.daniela@hakspittal.at>
2025-05-28 18:20:41 +0200

    anpassung fürs Bearbeiten

--- lendly/src/MediumDAO.java
+++ lendly/src/MediumDAO.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
@@ -9,11 +10,8 @@ public class MediumDAO {
 
         String sql = "INSERT INTO medium (titel, herausgeber, kategorie, gebuehr)  VALUES (?, ?, ?, ?);";
 
-            try (Connection conn = DatabaseConnector.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql)) {
-            //2. Schritt: Stmt mit Werten verknüpfen - für jedes Fragezeichen muss ein Wert mitgeschickt
-            //            werden.
-            // ACHTUNG, je nach Typ muss die richtige Setter-Funktion genutzt werden.
+        try (Connection conn = DatabaseConnector.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, medium.getTitel());
             stmt.setString(2, medium.getHerausgeber());
             stmt.setString(3, medium.getKategorie());
@@ -27,4 +25,41 @@ public class MediumDAO {
             JOptionPane.showMessageDialog(null, "Das Medium konnte leider nicht gespeichert werden.");
         }
     }//end speichereBestellung()
+
+    public boolean existiertID(int id){
+        String sql = "SELECT 1 FROM medium WHERE id = ? LIMIT 1";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                return rs.next();  // true wenn mindestens 1 Ergebnis da ist
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void bearbeiteMedium(Medium medium, int id){
+        String sql = "UPDATE medium SET titel = ?, herausgeber =? , kategorie = ?, gebuehr =? WHERE id = ?";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, medium.getTitel());
+            stmt.setString(2, medium.getHerausgeber());
+            stmt.setString(3, medium.getKategorie());
+            stmt.setDouble(4, medium.getGebuehr());
+            stmt.setInt(5, id);
+
+            stmt.executeUpdate();
+            System.out.println("Medium erfolgreich bearbeitet!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Das Medium konnte leider nicht bearbeitet werden.");
+        }
+
+    }
 }
