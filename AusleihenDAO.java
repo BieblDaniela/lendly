6df4c97a1b875f04e6694254893bcfdb12ed2377
BieblDaniela <biebl.daniela@hakspittal.at>
2025-06-14 09:59:58 +0200

    für zurückgeben vorbereitet

--- AusleihenDAO.java
+++ AusleihenDAO.java
@@ -17,10 +17,25 @@ public class AusleihenDAO {
                 stmt.setString(3, ausleihen.getDauer());
             
                 stmt.executeUpdate();
-                System.out.println("Ausleihen erfolgreich gespeichert!");
+                JOptionPane.showMessageDialog(null, "Medium wurde ausgeliehen.");
         } catch (SQLException e) {
             e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Die Bestellung konnte leider nicht gespeichert werden.");
+            JOptionPane.showMessageDialog(null, "Medium konnte nicht ausgeliehen werden.");
         }
-    }y
+    }
+
+    public void zuerueckgebenAusleihen(){
+
+        String sql = "DELETE FROM ausleihen WHERE ;";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            
+                stmt.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Medium wurde zurückgegeben.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Medium konnte nicht zurückgegeben werden.");
+        }
+    }
 }
